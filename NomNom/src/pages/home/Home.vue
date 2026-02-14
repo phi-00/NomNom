90b0a93c230ccbb3bb0cd2b3f956f956810a0b92
Ines Matos <inesgamer2108@gmail.com>
2026-02-14 00:46:35 +0000

    pages + home.vue

---
+++ NomNom/src/pages/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="home">
+    <h1>Bem-vindo ao NomNom!</h1>
+    <p>Esta é a página inicial do seu aplicativo.</p>
+  </section>
+</template>
+
+<script setup>
+// Adicione lógica aqui se necessário
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh;
+}
+h1 {
+  color: #42b883;
+}
+</style>
