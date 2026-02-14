9315c16efc924c30741bfa15308039f43307b355
Patrícia Bastos <patriciadanibastos@gmail.com>
2026-02-14 09:36:19 +0000

    check auth funciona

--- NomNom/src/components/Navbar.vue
+++ NomNom/src/components/Navbar.vue
@@ -67,11 +67,8 @@ onMounted(() => {
   window.addEventListener('storage', checkAuth);
 });
 
-<<<<<<< Updated upstream
 // Monitorar mudanças de rota para atualizar autenticação
-=======
 // Recheck auth when route changes
->>>>>>> Stashed changes
 watch(() => route.path, () => {
   checkAuth();
 });
